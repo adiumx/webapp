76dbfd7a6fa7326b4bf5270305b17449b0fe6955
adium <cesar.ruiz.f@hotmail.com>
2022-04-29 16:26:09 -0500

    version2.1

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 const app = require('./app')
 
 
-mongoose.connect('mongodb://localhost:27017/Paginas', (err, res)=>{
+mongoose.connect(port, (err, res)=>{
   if(err) throw err
   console.log('conección a la base de datos establecida')
   app.listen(port, ()=>{
